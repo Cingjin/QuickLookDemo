198751cce6845820ed19ec318cfdd1fae1a12527
yuxingjin <15327288437@163.com>
2020-01-21 11:06:23 +0800

    QuickLook 文件预览Demo

--- QuickLookDemo/ViewController.swift
+++ QuickLookDemo/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import QuickLook
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,QLPreviewControllerDelegate,QLPreviewControllerDataSource {
+    
+    // 预览控制
+    fileprivate var preViewController = QLPreviewController.init()
+    // 文件路径
+    fileprivate var filePath = Bundle.main.path(forResource: "test", ofType: ".docx")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        preViewController.delegate = self
+        preViewController.dataSource = self
+        preViewController.view.frame = self.view.frame;
+        self.view.addSubview(preViewController.view)
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self .present(preViewController, animated: true) {
+            
+        }
+    }
+    
+    // MARK: - QLPreviewControllerDelegate
+    
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        return 1
+    }
+    
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        return  URL.init(fileURLWithPath: filePath!) as QLPreviewItem
+    }
+    
+    
 }
 
