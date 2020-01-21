6b5d833fba60af0c809b0dd9b8792f45144e0cfe
yuxingjin <15327288437@163.com>
2020-01-21 10:42:05 +0800

    Initial Commit

---
+++ QuickLookDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickLookDemo
+//
+//  Created by Anmo on 2020/1/21.
+//  Copyright © 2020 com.Cingjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
